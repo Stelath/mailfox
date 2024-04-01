816f1d1802894a9f6a1994175690d414e29c4b62
Alex <korte@clemson.edu>
2024-04-01 13:57:10 -0400

    Improve Clustering Prediction

--- mailfox/vector/clustering.py
+++ mailfox/vector/clustering.py
@@ -1,28 +1,28 @@
 import pickle
 
-from sklearn.cluster import HBDSCAN
+from sklearn.cluster import HDBSCAN
 from sklearn.decomposition import PCA
 from sklearn.neighbors import NearestNeighbors
 
 class Clustering():
     def __init__(self, data, min_cluster_size=5):
         self.data = data
-        self.clusterer = HBDSCAN(min_cluster_size=min_cluster_size)
+        self.clusterer = HDBSCAN(min_cluster_size=min_cluster_size, store_centers='centroid', cluster_selection_epsilon=0.5)
         self.pca = PCA(n_components=0.95, svd_solver='auto')
         
         self.data = self.pca.fit_transform(self.data)
     
     def fit(self):
-        self.hbdscan.fit(self.data)
+        self.clusterer.fit(self.data)
         
-    def approximate_predict(self, vector, *, threshold=0.5):
+    def predict(self, vector, *, threshold=0.5):
         vector = self.pca.transform(vector)
         
         nbrs = NearestNeighbors(n_neighbors=1).fit(self.clusterer.centroids_)
         distances, indices = nbrs.kneighbors(vector.reshape(1, -1))
-
+        
         if distances[0][0] < threshold:
-            return self.clusterer.labels_[indices[0][0]]
+            return indices[0][0]
         else:
             return -1  # Outlier
     
